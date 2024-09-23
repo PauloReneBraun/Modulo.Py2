17c534210b0ee33a92d2ccd3b3fa7c6e10574434
Paulo Braun - Frontend Developer - React <66573504+PauloReneBraun@users.noreply.github.com>
2024-09-23 11:39:28 +0100

    Add files via upload

---
+++ poo.py
@@ -0,0 +1,10 @@
+# Poo
+
+#classe exemplo 
+class Pessoa:
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+    def imprimir(self):
+        print(f'Nome: {self.nome}, Idade: {self.idade}')
