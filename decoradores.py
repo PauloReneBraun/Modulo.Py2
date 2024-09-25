910fe0be50c47b2b5f94e7643082e1ea1c2f6621
Paulo Braun - Frontend Developer - React <66573504+PauloReneBraun@users.noreply.github.com>
2024-09-25 16:52:46 +0100

    Add files via upload

--- decoradores.py
+++ decoradores.py
@@ -8,3 +8,18 @@ def meu_decorador(func):
 @meu_decorador
 def minha_funcao():
     print('Estou sendo executada')
+
+minha_funcao()
+
+class MeuDecorador:
+    def __init__(self, func):
+        self.func = func
+
+    def __call__(self):
+        print('Estou antes da execução da função')
+        self.func()
+        print('Estou depois da execução da função')
+
+@MeuDecorador
+def segunda_funcao():
+    print('Estou sendo executada')
