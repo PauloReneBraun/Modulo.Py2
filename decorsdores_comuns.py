cf8bac478e278b4aa3fdf8815e5a96a5feb191bf
Paulo Braun - Frontend Developer - React <66573504+PauloReneBraun@users.noreply.github.com>
2024-09-26 16:34:12 +0100

    Add files via upload

--- decorsdores_comuns.py
+++ decorsdores_comuns.py
@@ -11,7 +11,13 @@ class MinhaClasse:
   @classmethod
   def metodo_classe(cls):
     return f"Metodo de classe {cls}"
+  
+  @staticmethod
+  def metodo_estatico():
+    return "Metodo estatico"
 
 obj = MinhaClasse(nome="Exemplo")
 print(obj.metodo_instancia())
 print(MinhaClasse.valor)
+print(MinhaClasse.metodo_classe())
+print(MinhaClasse.metodo_estatico())
